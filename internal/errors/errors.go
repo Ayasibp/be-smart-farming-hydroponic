262003a3883657d38bd9e9edeecc67bb6f79916e
eputrap <eputrap@gmail.com>
2025-01-25 20:13:41 +0700

    add ErrorGeneratingHashedPassword

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -12,10 +12,11 @@ var (
 
 	InvalidRequestBody = errors.New("invalid request body")
 
-	EmailAlreadyUsed          = errors.New("email already used")
-	UsernameAlreadyUsed       = errors.New("username already used")
-	PasswordDoesntMatch       = errors.New("password doesn't match")
-	PasswordContainUsername   = errors.New("password must not contain username")
-	PasswordSameAsBefore      = errors.New("Password cannot be same as before")
-	UsernamePasswordIncorrect = errors.New("username or password incorrect")
+	EmailAlreadyUsed              = errors.New("email already used")
+	UsernameAlreadyUsed           = errors.New("username already used")
+	PasswordDoesntMatch           = errors.New("password doesn't match")
+	PasswordContainUsername       = errors.New("password must not contain username")
+	PasswordSameAsBefore          = errors.New("Password cannot be same as before")
+	UsernamePasswordIncorrect     = errors.New("username or password incorrect")
+	ErrorGeneratingHashedPassword = errors.New("Error Generating Hashed Password")
 )
