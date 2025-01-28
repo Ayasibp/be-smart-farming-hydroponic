deaee8c2706f5aafffeb3895976a33f25213a920
eputrap <eputrap@gmail.com>
2025-01-28 22:11:41 +0700

    add AccountId

--- internal/model/profile.go
+++ internal/model/profile.go
@@ -9,6 +9,7 @@ import (
 
 type Profile struct {
 	ID        uuid.UUID      `json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	AccountId uuid.UUID      `json:"id" gorm:"type:uuid"`
 	Name      string         `json:"name" gorm:"type:varchar;not null;unique"`
 	Address   string         `json:"address" gorm:"type:varchar;not null"`
 	CreatedAt time.Time      `json:"created_at"`
