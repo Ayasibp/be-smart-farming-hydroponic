cff5362fd6aff69dae05412dc062cdc1caf073e6
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-02-03 10:36:40 +0700

    add delete system unit

--- internal/repository/system_unit_repository.go
+++ internal/repository/system_unit_repository.go
@@ -3,12 +3,14 @@ package repository
 import (
 	"time"
 
+	errs "github.com/Ayasibp/be-smart-farming-hydroponic/internal/errors"
 	"github.com/Ayasibp/be-smart-farming-hydroponic/internal/model"
 	"gorm.io/gorm"
 )
 
 type SystemUnitRepository interface {
 	CreateSystemUnit(inputModel *model.SystemUnit) (*model.SystemUnit, error)
+	DeleteSystemUnitById(inputModel *model.SystemUnit) (*model.SystemUnit, error)
 }
 
 type systemUnitRepository struct {
@@ -22,11 +24,25 @@ func NewSystemUnitRepository(db *gorm.DB) SystemUnitRepository {
 }
 
 func (r systemUnitRepository) CreateSystemUnit(inputModel *model.SystemUnit) (*model.SystemUnit, error) {
-	res := r.db.Raw("INSERT INTO hydroponic_system.system_units (farm_id ,unit_key, tank_volume , tank_a_volume , tank_b_volume , created_at) VALUES (?,?,?,?,?,?) RETURNING *;", inputModel.FarmId, inputModel.UnitKey, inputModel.TankVolume, inputModel.TankAVolume, inputModel.TankBVolume, time.Now()).Scan(inputModel)
+	res := r.db.Raw("INSERT INTO hydroponic_system.system_units (farm_id ,unit_key, tank_volume , tank_a_volume , tank_b_volume , created_at) VALUES (?,?,?,?,?,?) RETURNING *;", inputModel.FarmId, inputModel.UnitKey, inputModel.TankVolume, inputModel.TankAVolume, inputModel.TankBVolume, time.Now()).Scan(&inputModel)
 
 	if res.Error != nil {
 		return nil, res.Error
 	}
 	return inputModel, nil
 
+}
+
+func (r systemUnitRepository) DeleteSystemUnitById(inputModel *model.SystemUnit) (*model.SystemUnit, error) {
+
+	res := r.db.Raw("UPDATE hydroponic_system.system_units SET deleted_at = ? WHERE id = ? RETURNING *", time.Now(), inputModel.ID).Scan(&inputModel)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	if res.RowsAffected == 0 {
+
+		return nil, errs.InvalidSystemUnitID
+	}
+	return inputModel, nil
 }
--- internal/routes/route.go
+++ internal/routes/route.go
@@ -39,6 +39,7 @@ func Build(srv *gin.Engine, h Handlers, middleware Middlewares) {
 
 	systemUnit := srv.Group("/system")
 	systemUnit.POST("/create", h.SystemUnit.CreateSystemUnit)
+	systemUnit.DELETE("/:systemId", h.SystemUnit.DeleteSystemIdById)
 
 	// super admin
 	authSuper := srv.Group("/auth-super")
