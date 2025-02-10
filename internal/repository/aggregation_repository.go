94c068347b98a61ac733d6d3d2269ab4837420e2
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-02-10 15:56:45 +0700

    add create aggregation batch

--- internal/repository/aggregation_repository.go
+++ internal/repository/aggregation_repository.go
@@ -5,6 +5,7 @@ import (
 )
 
 type AggregationRepository interface {
+	CreateAggregationBatch(inputValuseString *string) (int, error)
 }
 
 type aggregationRepository struct {
@@ -15,4 +16,20 @@ func NewAggregationRepository(db *gorm.DB) AggregationRepository {
 	return &aggregationRepository{
 		db: db,
 	}
+}
+
+func (r aggregationRepository) CreateAggregationBatch(inputValuseString *string) (int, error) {
+
+	var outputModel *int
+
+	sqlScript := `INSERT INTO hydroponic_system.aggregations(farm_id, system_id, name, value, time_range, activity,time, created_at) 
+				VALUES ` + *inputValuseString +
+		` RETURNING 1;`
+
+	res := r.db.Raw(sqlScript).Scan(&outputModel)
+
+	if res.Error != nil {
+		return 0, res.Error
+	}
+	return 1, nil
 }
--- internal/service/aggregation_service.go
+++ internal/service/aggregation_service.go
@@ -1,22 +1,30 @@
 package service
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/Ayasibp/be-smart-farming-hydroponic/internal/repository"
 )
 
 type AggregationService interface {
+	CreateBatchAggregateGrowthHistMonthly() (bool, error)
 }
 
 type aggregationService struct {
 	aggregationRepo repository.AggregationRepository
 	farmRepo        repository.FarmRepository
 	systemUnitRepo  repository.SystemUnitRepository
+	growthHistRepo  repository.GrowthHistRepository
 }
 
 type AggregationServiceConfig struct {
 	AggregatoionRepo repository.AggregationRepository
 	FarmRepo         repository.FarmRepository
 	SystemUnitRepo   repository.SystemUnitRepository
+	GrowthHistRepo   repository.GrowthHistRepository
 }
 
 func NewAggregationService(config AggregationServiceConfig) AggregationService {
@@ -24,5 +32,34 @@ func NewAggregationService(config AggregationServiceConfig) AggregationService {
 		aggregationRepo: config.AggregatoionRepo,
 		farmRepo:        config.FarmRepo,
 		systemUnitRepo:  config.SystemUnitRepo,
+		growthHistRepo:  config.GrowthHistRepo,
 	}
+}
+
+func (s aggregationService) CreateBatchAggregateGrowthHistMonthly() (bool, error) {
+
+	// minus today
+	aggregatesVal, err := s.growthHistRepo.GetAggregateMonthly()
+	if err != nil {
+		return false, err
+	}
+
+	var batchValues string
+
+	for i := 0; i < len(aggregatesVal); i++ {
+		val := aggregatesVal[i]
+		for key, value := range val.AggregatedValues {
+			batchValues = batchValues + "(" + "'" + val.FarmId.String() + "'," + "'" + val.SystemId.String() + "'," + "'growth-hist'" + "," + fmt.Sprintf("%.2f", value) + "," + "'monthly'" + ",'" + key + "'," + "'" + strconv.Itoa(val.Year) + "-" + strconv.Itoa(val.Month) + "-1" + "'" + ",'" + time.Now().Format("2006-01-02 15:04:05") + "')" + ","
+		}
+	}
+
+	batchValues = strings.TrimSuffix(batchValues, ",")
+
+	_, err = s.aggregationRepo.CreateAggregationBatch(&batchValues)
+	if err != nil {
+		return false, nil
+	}
+
+	return true, nil
+
 }
